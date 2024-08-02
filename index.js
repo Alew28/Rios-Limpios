3fd3e3c0070bf25e5cccd3267cbb494afa878910
Alew28 <alejandrobonetti28@gmail.com>
2024-08-02 18:45:16 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let slides = document.getElementsByClassName("slide");
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+let ubicacionPrincipal = window.scrollY
+window.onscroll = function() {
+  let desplazamientoActual = window.scrollY;
+  if (ubicacionPrincipal >= desplazamientoActual){
+    document.getElementById('navbar').style.top = '0';
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides, 3000); // Cambia la imagen cada 3 segundos
+  else{
+    document.getElementById('navbar').style.top = '-15vh';
+  }
+  ubicacionPrincipal = desplazamientoActual;
 }
